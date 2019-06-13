d1a9b58122c99b3f82d5a91b99543c2ec8dfd6bc
hendisantika <hendisantika@yahoo.co.id>
2019-06-13 07:10:01 +0700

    #8 - Add HomeController

---
+++ src/main/java/com/hendisantika/coinmarketcaprestapiconsumer/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.hendisantika.coinmarketcaprestapiconsumer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : coin-marketcap-rest-api-consumer
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-06-13
+ * Time: 07:09
+ */
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String getHomePage() {
+        return "index";
+    }
+}
